866d22aae1e6692aef463a7d2b3d5b65caeaf90b
HongJoonHyung <sesa0802@naver.com>
2021-11-18 04:08:29 +0900

    css 적용중

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Result from './components/result';
 import Api from './components/api';
-import User from './components/user';
+import User from './components/home';
 import Example from './components/example';
 import Test from './components/test';
 
@@ -11,6 +11,14 @@ function App() {
     return (
     <div className="App">
       <Router>
+      <div className="header">
+        <nav>
+          <h1>엘리스 심리적성상담</h1>
+        </nav>
+      </div>
+
+      <div className = "main">
+      
         {/* 시작 페이지 */}
         {/* <Link to="/index">index</Link> 초기화면 */}
         
@@ -26,6 +34,10 @@ function App() {
           <Route path="/test/end" component={Test}/>              {/* 검사완료 페이지 */}
           <Route path="/result" component={Result}/>     {/* 결과표 페이지 */}
         </Switch>
+      
+      </div>
+      <div className = "footer">
+      </div>
       </Router>
     </div>
   );
